36ac01576d5dbbd410de9156a66bb5a8e7e17262
Hisora03 <sunsky2328@gmail.com>
2026-08-11 22:43:36 +0900

    タスク２＿

--- 2026_summer_Task/summer_Task_2_松岡.cpp
+++ 2026_summer_Task/summer_Task_2_松岡.cpp
@@ -9,28 +9,28 @@ int main()          //メイン
     int player;    //プレイヤーの手
     int cpu;       //敵の手
     int result;    //結果
-    int i;
-    int win = 0;
-    int lose = 0;
-    int draw = 0;
+    int i;         
+    int win = 0;   //勝利数
+    int lose = 0;  //敗北数
+    int draw = 0;  //引き分け数
 
-    srand((unsigned int)time(nullptr));
+    srand((unsigned int)time(nullptr));   //乱数初期化
 
     cout << "じゃんけんゲームを開始します。" << endl;
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)  //５回勝負
     {
         cout << endl;
-        cout << "-------- " << i + 1 << "回戦 --------" << endl;
+        cout << "-------- " << i + 1 << "回戦 --------" << endl;    //回数表示
 
-        while (true)
+        while (true)    //ゲームループ
         {
             cout << "0：グー　1：チョキ　2：パー" << endl;
             cout << "プレイヤーの手を入力してください：" << endl;
 
-            cin >> player;
+            cin >> player;   //プレイヤー入力
 
-            if (player >= 0 && player <= 2)
+            if (player >= 0 && player <= 2)   //入力が誤ってる場合
             {
                 break;
             }
@@ -38,53 +38,53 @@ int main()          //メイン
             cout << "入力に誤りがあります。再入力してください。" << endl;
         }
 
-        cpu = rand() % 3;
+        cpu = rand() % 3;   //CPUの手をランダムで決定
 
-        switch (player)
+        switch (player)  //手の表記
         {
         case 0:
-            cout << "PLAYER：グー" << endl;
+            cout << "PLAYER：グー" << endl;   //グー
             break;
 
         case 1:
-            cout << "PLAYER：チョキ" << endl;
+            cout << "PLAYER：チョキ" << endl;  //チョキ
             break;
 
         case 2:
-            cout << "PLAYER：パー" << endl;
+            cout << "PLAYER：パー" << endl;    //パー
             break;
         }
 
 
-        switch (cpu)
+        switch (cpu)  //CPUの手の表示
         {
         case 0:
-            cout << "CPU：グー" << endl;
+            cout << "CPU：グー" << endl;  //グー
             break;
 
         case 1:
-            cout << "CPU：チョキ" << endl;
+            cout << "CPU：チョキ" << endl; //チョキ
             break;
 
         case 2:
-            cout << "CPU：パー" << endl;
+            cout << "CPU：パー" << endl;  //パー
             break;
         }
 
 
-        result = player - cpu;
+        result = player - cpu;    //結果＝プレイヤーの手-CPUの手
 
-        if (result == -1 || result == 2)
+        if (result == -1 || result == 2)  //プレイヤーが勝つ
         {
             cout << "PLAYER WIN!" << endl;
             win++;
         }
-        else if (result == 0)
+        else if (result == 0)   //引き分け
         {
             cout << "DRAW" << endl;
             draw++;
         }
-        else
+        else                   //CPUが勝つ
         {
             cout << "CPU WIN!" << endl;
             lose++;
@@ -95,7 +95,7 @@ int main()          //メイン
     cout << endl;
     cout << "==============================" << endl;
     cout << "5回勝負終了！" << endl;
-    cout << win << "勝 "
+    cout << win << "勝 "     //勝敗表示
         << draw << "分 "
         << lose << "敗" << endl;
     cout << "==============================" << endl;
