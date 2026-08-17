77ad45ca2ef74eb54ffff96601c9309f2e1d1b06
senahomura <senahomura@gmail.com>
2026-08-17 12:18:26 +0900

    task3

---
+++ jackpot/20260817_function_松岡陽空.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+using namespace std;
+
+#include"20260817_header_¼‰ª—z‹ó.h"
---
+++ jackpot/20260817_main_松岡陽空.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include"20260817_header_¼‰ª—z‹ó.h"
+using namespace std;
+
+//’è”
+
+int main(void)
+{
+	//•Ï”
+	//—”‰Šú‰»
+	//
+	return 0;
+}
+
